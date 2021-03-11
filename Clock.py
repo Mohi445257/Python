90179c789cd9e0cc7455c4c3438f438fd00a54c8
mohi445257 <mohi445257@gmail.com>
2021-03-11 14:26:07 +0600

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

---
+++ Clock.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+
+root= Tk()
+root.title("Clock")
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    lbl.config(text = string)
+    lbl.after(1000, time)
+
+
+lbl = Label (root,font =("ds-digital" ,80, "bold"),foreground ="Black", background = "cyan")
+lbl.pack(anchor = "center" )
+time()
+mainloop()
+
